5db41244b7728a242cba2a9199935567cad9e25d
RITESH KUMAR <73475783+riteshtheone@users.noreply.github.com>
2025-06-30 13:21:21 +0530

    Update conform.lua

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,32 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        -- css = { "prettier" },
+        -- html = { "prettier" },
+    },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    formatters = {
+        -- C & C++
+        ["clang-format"] = {
+            prepend_args = {
+                "-style={ \
+                        IndentWidth: 4, \
+                        TabWidth: 4, \
+                        UseTab: Never, \
+                        AccessModifierOffset: 0, \
+                        IndentAccessModifiers: true, \
+                        PackConstructorInitializers: Never}",
+            },
+        },
+    },
+
+    -- format_on_save = {
+    --   -- These options will be passed to conform.format()
+    --   timeout_ms = 500,
+    --   lsp_fallback = true,
+    -- },
 }
 
 return options
