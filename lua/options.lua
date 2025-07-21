c50ae9804b88143623b1ff6a98bcab155ff948a2
Ritesh Kumar <riteshnagwan@gmail.com>
2025-07-21 16:29:11 +0530

    updated

--- lua/options.lua
+++ lua/options.lua
@@ -2,15 +2,8 @@ require "nvchad.options"
 
 local o = vim.o
 
--- o.modeline = false
-
--- Indenting
 o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
 
-
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt ='both' -- to enable cursorline!
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,33 +1,54 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require "configs.treesitter"
-    end,
-  },
-
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
     },
-  },
 
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require "configs.treesitter"
+        end,
+    },
+
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+    },
+
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = true,
+        keys = {
+            {
+                "<M-t>", -- Alt + t
+                function()
+                    require("toggleterm").toggle(nil, nil, nil, "float")
+                end,
+                mode = { "n", "i", "v", "t" }, -- Enable in all modes
+                desc = "Toggle Floating Terminal (Alt+t)",
+            },
+        },
+    },
+
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^6", -- Recommended
+        lazy = false, -- This plugin is already lazy
+    },
 }
